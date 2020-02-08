c14fa4677333aa131349be171bd4e3fb404a2814
aeaells <seiha.iphone@gmail.com>
2020-02-08 15:31:54 +0900

    pod install

---
+++ alpha_message/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alpha_message
+//
+//  Created by aeaells on 2020/02/08.
+//  Copyright © 2020 seiha i. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
